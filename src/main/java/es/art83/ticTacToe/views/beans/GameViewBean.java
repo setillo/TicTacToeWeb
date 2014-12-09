55f37b57a16e23774625d716201f5b17a11dfce9
miw-upm <miw@eui.upm.es>
2014-12-09 13:42:22 +0100

    View ShowGame

--- src/main/java/es/art83/ticTacToe/views/beans/GameViewBean.java
+++ src/main/java/es/art83/ticTacToe/views/beans/GameViewBean.java
@@ -1,5 +1,6 @@
 package es.art83.ticTacToe.views.beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,10 @@ import org.apache.logging.log4j.LogManager;
 import es.art83.ticTacToe.controllers.CreateGameController;
 import es.art83.ticTacToe.controllers.LogoutController;
 import es.art83.ticTacToe.controllers.OpenGameController;
+import es.art83.ticTacToe.controllers.PlaceCardController;
+import es.art83.ticTacToe.controllers.SaveGameController;
+import es.art83.ticTacToe.models.entities.CoordinateEntity;
+import es.art83.ticTacToe.models.utils.ColorModel;
 import es.art83.ticTacToe.models.utils.TicTacToeStateModel;
 
 @ManagedBean
@@ -21,12 +26,57 @@ public class GameViewBean extends ViewBean {
 
     private boolean openedGame;
 
+    private boolean gameNamed;
+
+    private String nameGame;
+
+    private String[][] fichas;
+
+    private boolean gameOver;
+
+    private ColorModel winner;
+
+    private boolean savedGame;
+
+    private ColorModel turn;
+
+    private boolean sixCardOnBoard;
+
+    private List<CoordinateEntity> validSourceCoordinates;
+    
+    private String selectedSourceCoordinate;
+
+    private List<CoordinateEntity> validDestinationCoordinates;
+
+    private String selectedDestinationCoordinate;
+
     @PostConstruct
     public void update() {
         this.openedGame = this.getControllerFactory().getTicTacToeStateModel() == TicTacToeStateModel.OPENED_GAME;
         this.gameNames = this.getControllerFactory().getStartGameController().readGameNames();
+        // Se actualiza nameGame
+        this.gameNamed = true;
+        this.nameGame = "Juego...";
+        // Se actualiza fichas
+        this.fichas = new String[][] { {"--", "X", "--"}, {"X", "--", "--"}, {"O", "--", "--"}};
         LogManager.getLogger(OpenGameController.class.getName()).info(
                 "Open game: consultando partidas" + this.gameNames);
+        // Se actualiza winner
+        this.gameOver = false;
+        this.winner = ColorModel.O;
+        // Se actualiza si la partida esta salvada
+        this.savedGame = false;
+        // se actualiza turno
+        this.turn = ColorModel.O;
+        // Se actualiza validSourceCoordinates
+        this.sixCardOnBoard = true;
+        this.validSourceCoordinates = new ArrayList<>();
+        this.validSourceCoordinates.add(new CoordinateEntity(0, 0));
+        this.validSourceCoordinates.add(new CoordinateEntity(0, 1));
+        // Se actualiza validDestinationCoordinates
+        this.validDestinationCoordinates = new ArrayList<>();
+        this.validDestinationCoordinates.add(new CoordinateEntity(1, 0));
+        this.validDestinationCoordinates.add(new CoordinateEntity(1, 1));
     }
 
     public String getGameNameSelected() {
@@ -45,10 +95,67 @@ public class GameViewBean extends ViewBean {
         return this.openedGame;
     }
 
-    public boolean hasGames() {
-        return this.gameNames.size() > 0;
+    public boolean isZeroGameNames() {
+        return this.gameNames.size() == 0;
     }
 
+    public boolean isGameNamed() {
+        return gameNamed;
+    }
+
+    public String getNameGame() {
+        return this.nameGame;
+    }
+
+    public String[][] getFichas() {
+        return fichas;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public ColorModel getWinner() {
+        return winner;
+    }
+
+    public boolean isSavedGame() {
+        return savedGame;
+    }
+
+    public ColorModel getTurn() {
+        return turn;
+    }
+
+    public boolean isSixCardOnBoard() {
+        return sixCardOnBoard;
+    }
+
+    public List<CoordinateEntity> getValidSourceCoordinates() {
+        return validSourceCoordinates;
+    }
+
+    public String getSelectedSourceCoordinate() {
+        return selectedSourceCoordinate;
+    }
+
+    public void setSelectedSourceCoordinate(String selectedSourceCoordinate) {
+        this.selectedSourceCoordinate = selectedSourceCoordinate;
+    }
+
+    public List<CoordinateEntity> getValidDestinationCoordinates() {
+        return validDestinationCoordinates;
+    }
+
+    public String getSelectedDestinationCoordinate() {
+        return selectedDestinationCoordinate;
+    }
+
+    public void setSelectedDestinationCoordinate(String selectedDestinationCoordinate) {
+        this.selectedDestinationCoordinate = selectedDestinationCoordinate;
+    }
+
+    // Process
     public String createGame() {
         this.getControllerFactory().getCreateGameControler().createGame();
         this.openedGame = true;
@@ -59,6 +166,16 @@ public class GameViewBean extends ViewBean {
     public String openGame() {
         LogManager.getLogger(OpenGameController.class.getName()).info(
                 "Open game: " + this.gameNameSelected);
+        return null;
+    }
+
+    public String saveGame() {
+        LogManager.getLogger(SaveGameController.class.getName()).info("Save game");
+        return null;
+    }
+
+    public String placeCard() {
+        LogManager.getLogger(PlaceCardController.class.getName()).info("Place card");
         return null;
     }
 
